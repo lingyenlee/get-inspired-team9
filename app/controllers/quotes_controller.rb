40fc4a492d742800f7cbc7d9adc53913b424af5e
lingyenlee <lingyenlee@gmail.com>
2020-05-07 11:59:45 +1000

    add notices

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,6 @@
 class QuotesController < ApplicationController
 
+    before_action :authenticate_user!
     before_action :set_quote, only: [:show, :edit, :update, :destroy ]
 
     def index
@@ -15,17 +16,29 @@ class QuotesController < ApplicationController
     end
 
     def create
+        
         @quote = current_user.quotes.create(quote_params)
 
-        if @quote.errors.any?
-            render "new"
-        else
-            redirect_to quotes_path
-        end 
+        respond_to do |format|
+            if @quote.save
+              format.html { redirect_to @quote, notice: 'Toy was successfully created.' }
+              format.json { render :show, status: :created, location: @quote }
+            else
+              format.html { render :new }
+              format.json { render json: @quote.errors, status: :unprocessable_entity }
+            end
+          end
+
+
+        # if @quote.errors.any?
+        #     render "new"
+        # else
+        #     format.html { redirect_to @quote, notice: 'Quote was successfully created.' }
+        #     format.json { render :show, status: :created, location: @quote }
+        # end 
     end
 
     def edit
-       
         if @quote
             render "edit"
         else
@@ -34,25 +47,25 @@ class QuotesController < ApplicationController
     end
 
     def update
-       
-        if @quote
-            @updated_quote = Quote.update(quote_params)
-            if @quote.errors.any?
-                render "edit"
+        respond_to do |format|
+            if @quote.update(quote_params)
+              format.html { redirect_to @quote, notice: 'Changes successfully updated.' }
+              format.json { render :show, status: :ok, location: @quote }
             else
-                redirect_to quotes_path
-            end 
-        else 
-            redirect_to quotes_path
-        end
+              format.html { render :edit }
+              format.json { render json: @quote.errors, status: :unprocessable_entity }
+            end
+          end
     end
 
     def destroy
         if @quote 
             @quote.destroy
+            respond_to do |format|
+                format.html { redirect_to quotes_path, notice: 'Quote was successfully destroyed.' }
+                format.json { head :no_content }
+            end
         end
-        
-        redirect_to quotes_path
     end
 
     private
